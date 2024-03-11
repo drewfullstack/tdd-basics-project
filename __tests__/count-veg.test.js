570a4c729d5f6531a0d97b4c011453c1929f0a62
Andrew Lowes <drewfullstack@gmail.com>
2024-03-11 11:37:30 +0000

    completes count-veg function

---
+++ __tests__/count-veg.test.js
@@ -0,0 +1,42 @@
+const countVeg = require("../katas/count-veg");
+
+/* base case - if type input is empty array, return []
+ case 2 - if veg type is empty string, return 0
+
+example
+countVeg([
+  {name: 'Parsnip', type: 'root', quantity: 4},
+  {name: 'Broccoli', type: 'brassica', quantity: 1},
+  {name: 'Carrot', type: 'root', quantity: 5},
+  {name: 'Onion', type: 'bulb', quantity: 3},
+  {name: 'Chard', type: 'leaf', quantity: 3},
+  {name: 'Runner beans', type: 'legume', quantity: 8}
+], 'root') should return 9
+*/
+
+describe("countVeg", () => {
+  test("if input is empty array, return 0", () => {
+    const output = countVeg([]);
+    expect(output).toEqual(0);
+  });
+
+  test("if veg type is empty string, return 0", () => {
+    const output = countVeg([], "");
+    expect(output).toBe(0);
+  });
+
+  test("returns correct quantity for given veg type", () => {
+    const output = countVeg(
+      [
+        { name: "Parsnip", type: "root", quantity: 4 },
+        { name: "Broccoli", type: "brassica", quantity: 1 },
+        { name: "Carrot", type: "root", quantity: 5 },
+        { name: "Onion", type: "bulb", quantity: 3 },
+        { name: "Chard", type: "leaf", quantity: 3 },
+        { name: "Runner beans", type: "legume", quantity: 8 },
+      ],
+      "root"
+    );
+    expect(output).toBe(9);
+  });
+});
--- katas/count-veg.js
+++ katas/count-veg.js
@@ -1,3 +1,20 @@
-function countVeg() {}
+function countVeg(vegArray, vegType) {
+  if (vegArray.length === 0) {
+    return 0;
+  }
+  if (vegType === "") {
+    return 0;
+  }
+
+  const filteredVeg = vegArray.filter((vegObj) => {
+    if (vegObj.type === vegType) {
+      return vegObj;
+    }
+  });
+
+  let count = 0;
+  filteredVeg.forEach((veg) => (count += veg.quantity));
+  return count;
+}
 
 module.exports = countVeg;
